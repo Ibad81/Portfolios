bb87035e8924c6d5e08f887e51600502528a1417
ibad81 <ibadbaig921@gmail.com>
2024-08-06 20:24:24 +0530

    Final- some step

--- Portfolio/src/sections/Hero/Hero.jsx
+++ Portfolio/src/sections/Hero/Hero.jsx
@@ -25,7 +25,7 @@ function Hero() {
         <img
           src={heroImg}
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Profile picture of Ibad Baig"
         />
         <img
           className={styles.colorMode}
@@ -36,19 +36,16 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Harris
-          <br />
-          Johnsen
+          IBAD
+        
+          BAIG
         </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
-          </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/Ibad81" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.linkedin.com/in/ibad-baig-06226b1ba/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
